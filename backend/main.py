bebdc2a6c3692be3e1ab21bb71279d5ab40cc205
nitish680 <sainnitish488@gmail.com>
2026-06-22 18:50:33 +0530

    fix: enable CORS for frontend backend communication

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,40 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=[
+        "http://127.0.0.1:5501",
+        "http://localhost:5501"
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
     return {
         'message':'this is the backend of thwe project'}
+@app.post("/chat")
+def chat(data:dict):
+    question=data["question"]
+    return {
+        "answer":f"you asker {question}"
+    }
+
+
+
+
+
+
+
+
+
+
+
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,1 +1,34 @@
-console.log("RAG Document frontend loaded seccussfully")
+
+async function askQuestion(){
+
+    let question =
+    document.getElementById("question").value;
+
+
+    let response = await fetch(
+        "http://127.0.0.1:8000/chat",
+        {
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                question:question
+
+            })
+
+        }
+    );
+
+
+    let data = await response.json();
+
+
+    document.getElementById("chatBox").innerHTML =
+    data.answer;
+
+}
